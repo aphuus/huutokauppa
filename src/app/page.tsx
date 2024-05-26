84e5bb958e40292756b2ff6176053f601c9bdb5d
aphuus <aphuus@utu.fi>
2024-05-26 11:09:51 +0300

    next-auth and items table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,39 @@
-import { database } from "@/db/database";
-import { bids as bidsSchema } from "@/db/schema";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { revalidatePath } from "next/cache";
 
+import { auth } from "@/auth";
+import { database } from "@/db/database";
+import { items } from "@/db/schema";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+
 export default async function HomePage() {
-  const bids = await database.query.bids.findMany();
+  const session = await auth();
+
+  const allItems = await database.query.items.findMany();
 
   return (
-    <main className="container mx-auto px-[3%]">
+    <main className="container mx-auto px-[5%] py-12">
+      {session ? <SignOut /> : <SignIn />}
+
       <form
         action={async (formData: FormData) => {
           "use server";
 
-          await database.insert(bidsSchema).values({});
+          await database.insert(items).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
           revalidatePath("/");
         }}
       >
-        <Input name="bid" placeholder="Bid" />
-        <Button type="submit">Huuda</Button>
+        <Input name="name" placeholder="Nimeä myytävä kohde..." />
+        <Button type="submit">Listaa myytäväksi</Button>
       </form>
 
-      {bids.map((bid) => (
-        <div key={bid.id}>{bid.id}</div>
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
       ))}
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "tailwindcss";
 
-const { fontFamily } = require("tailwindcss/defaultTheme");
-
 const config = {
   darkMode: ["class"],
   content: [
@@ -21,7 +19,8 @@ const config = {
     },
     extend: {
       fontFamily: {
-        sans: ["var(--font-sans)", ...fontFamily.sans],
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
       },
       colors: {
         border: "hsl(var(--border))",
